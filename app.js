f08808f54dd62c1d9312584c5bb5c548a6c0e779
LesJudge <birolacko82@gmail.com>
2018-01-12 17:33:27 +0100

    This is a test add.

---
+++ app.js
@@ -0,0 +1,23 @@
+// require and instantiate express
+const app = require('express')()
+
+var data = require('./model/data.json');
+var assignment = require('./model/assignment.json');
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// index page
+app.get('/', function (req, res) {
+    res.render('index', {
+        data: data,
+        assignment: assignment,
+    });
+});
+
+app.listen(8081);
+
+console.log('listening on port 8081');
+
+
+
